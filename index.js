3743a85a650bf1d234dd271c457a45972c6995a8
Nathu03 <Narthanan.s@yahoo.com>
2023-09-19 11:08:08 +0530

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const simpleGit = require('simple-git');
+
+const FILE_PATH = './data.json';
+
+const DATE = moment().subtract(2, 'd').format();
+
+const data = {
+  date: DATE
+};
+
+jsonfile.writeFile(FILE_PATH, data, (err) => {
+  if (err) {
+    console.error('Error writing to data.json:', err);
+  } else {
+    console.log('Data written to data.json successfully.');
+  }
+});
